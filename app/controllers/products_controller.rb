95d06c1d29fc95a5fbe96735687928b50b0f70fd
Luis_Yos <130430932+LAYL96@users.noreply.github.com>
2023-08-09 11:12:29 -0600

    se agrego la ruta para eleminar, el metodo para eliminar luego el enlace para eliminar en la vista show y los test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,12 @@ class ProductsController < ApplicationController
         end
     end 
 
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+        redirect_to products_path, notice: 'Tu Producto se ha Eliminado Correctamente!', status: :see_other
+    end
+
     private
     def product_params
         params.require(:product).permit(:title, :description, :price)
