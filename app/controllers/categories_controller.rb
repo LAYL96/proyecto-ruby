71db4ac4bec5e3113a9f6d7d3e295c39dc812221
Luis_Yos <130430932+LAYL96@users.noreply.github.com>
2023-08-10 11:36:15 -0600

    correccion de erroes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,16 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show edit update destroy ]
-
-  # GET /categories or /categories.json
   def index
     @categories = Category.all
   end
 
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
   def edit
     category
   end
 
-  # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
 
@@ -27,7 +21,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
   def update
     if category.update(category_params)
       redirect_to categories_url, notice: t('.updated')
@@ -36,7 +29,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # DELETE /categories/1 or /categories/1.json
   def destroy
     category.destroy
 
@@ -44,12 +36,10 @@ class CategoriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def category
       @category = Category.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def category_params
       params.require(:category).permit(:name)
     end
