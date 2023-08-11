ca9060f870267c7ad48aafd3212f44f09b6aa18b
Luis_Yos <130430932+LAYL96@users.noreply.github.com>
2023-08-11 11:48:19 -0600

    se modifico el metodo index del controlador de productos para ordenar los productos, se realizo lo mismo para el metodo index de categorias, se modifico el modelo de categorias y de productos, se agrego el select en la vista _form, se agregaron los test en ambos controladores y fixtures

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,33 +1,43 @@
 require 'test_helper'
+
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test 'render a list of products' do
     get products_path
+
     assert_response :success
-    assert_select '.product', 2
+    assert_select '.product', 3
   end
+
   test 'render a detailed product page' do
     get product_path(products(:ps4))
+
     assert_response :success
     assert_select '.title', 'PS4 Fat'
     assert_select '.description', 'PS4 en buen estado'
     assert_select '.price', '150'
   end
+
   test 'render a new product form' do
     get new_product_path
+
     assert_response :success
     assert_select 'form'
   end
+
   test 'allows to create a new product' do
     post products_path, params: {
       product: {
         title: 'Nintendo 64',
         description: 'Le faltan los cables',
-        price: 45
+        price: 45,
+        category_id: categories(:videogames).id
       }
     }
+
     assert_redirected_to products_path
     assert_equal flash[:notice], 'Tu producto se ha creado correctamente'
   end
+
   test 'does not allow to create a new product with empty fields' do
     post products_path, params: {
       product: {
@@ -36,22 +46,28 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
         price: 45
       }
     }
+
     assert_response :unprocessable_entity
   end
+
   test 'render an edit product form' do
     get edit_product_path(products(:ps4))
+
     assert_response :success
     assert_select 'form'
   end
+
   test 'allows to update a product' do
     patch product_path(products(:ps4)), params: {
       product: {
         price: 165
       }
     }
+    
     assert_redirected_to products_path
     assert_equal flash[:notice], 'Tu producto se ha actualizado correctamente'
   end
+
   test 'does not allow to update a product with an invalid field' do
     patch product_path(products(:ps4)), params: {
       product: {
