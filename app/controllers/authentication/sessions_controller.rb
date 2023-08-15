8b654d23fc201f5cff37f0c24ff64c6358588e60
Luis_Yos <130430932+LAYL96@users.noreply.github.com>
2023-08-14 18:19:56 -0600

    34. se agrego proteccion a las paginas para que solo el usuario loguedo pueda acceder a ver categorias o crear categorias nuevas

--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -1,14 +1,17 @@
 class Authentication::SessionsController < ApplicationController
-  def new
-  end
+    skip_before_action :protect_pages
 
-  def create
-    @user = User.find_by("email = :login OR username = :login", { login: params[:login] })
-
-    if @user&.authenticate(params[:password])
-      redirect_to products_path, notice: t('.created')
-    else
-      redirect_to new_session_path, alert: t('.failed')
+    def new
     end
-  end
+
+    def create
+        @user = User.find_by("email = :login OR username = :login", { login: params[:login] })
+
+        if @user&.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to products_path, notice: t('.created')
+        else
+            redirect_to new_session_path, alert: t('.failed')
+        end
+    end
 end
