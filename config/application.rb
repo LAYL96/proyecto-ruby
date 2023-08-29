69016bf40125c9da098e7c0313f79ddaa10a0020
Luis_Yos <130430932+LAYL96@users.noreply.github.com>
2023-08-29 12:56:27 -0600

    53. se agrego lo que es un background job para que sea asincrono lo de encontrar el codigo de pais del usuario que se este registrando y la gema de sidekiq para hacerlo asincrono

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module TiendaECO
 
     # Allow multiquery
     config.active_record.async_query_executor = :global_thread_pool
+
+    # Background job
+    config.active_job.queue_adapter = :sidekiq
   end
 end
